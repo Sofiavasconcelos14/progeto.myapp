affa2c9b8b801e7622e54dc37bd2c8c17925cf3f
Sofiavasconcelos14 <sofiaconceicao363@gmail.com>
2026-03-24 17:26:51 -0400

    new

---
+++ src/app/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+    minhavar = "sofia"
+
+  lista = [
+ {id: 1, nome: 'html'},
+ {id: 1, nome: 'css'},
+ {id: 1, nome: 'JavaScript'},
+ {id: 1, nome: 'Angular'},
+ {id: 1, nome: 'Bootstrap'}
+ 
+
+  ]
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
