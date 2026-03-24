e8e4c0ff76cba03637ad3c22fdcf4a8330c638e2
Sofiavasconcelos14 <sofiaconceicao363@gmail.com>
2026-03-24 18:25:55 -0400

    origin

---
+++ app-vascon/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+
+export const routes: Routes = [
+    {
+        path: '' , 
+        component: Home
+    },
+
+    {
+        path: 'sobre',
+        component: About
+    }
+];
+
