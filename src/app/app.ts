e88125426dfc6ec6f993ec0e00caf483d93a7f25
Sofiavasconcelos14 <sofiaconceicao363@gmail.com>
2026-06-16 18:24:41 -0400

    novo novo

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,10 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-app');
-
-
-
 }
